fc947dec41794072e82121dd76bf39ff49de9ab9
denmak <denmak@live.ru>
2017-04-29 16:42:55 +0200

    first commit buch bot

---
+++ config/config.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+exampleconf='this is an example'
+token = '327670438:AAH30zDGavR6-Ht3u-lMOFR6F3xqJnSS2IU'
+email = 'denmak0383@gmail.com'
+password = '1592648@gmail.com'
+weight = '180'
+# Find this value in the url with 'key=XXX' and copy XXX below
+spreadsheet_key = '1-iKub0vWhJ4RTIyiKJ-F_J8FdEVFovilD258XJHURRw'
+# All spreadsheets have worksheets. I think worksheet #1 by default always
+# has a value of 'od6'
+worksheet_id = 'od6'
+
+clid='220360779726-9udn1qo31ie4096mbtg4k0hojog8llil.apps.googleusercontent.com'
+clsec='qNkn1n6BQDNir-ZEB-_CR_u2'
+
+discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+				'version=v4')
+spreadsheetId = '1-iKub0vWhJ4RTIyiKJ-F_J8FdEVFovilD258XJHURRw'
+value_input_option = u'RAW'  # TODO: Update placeholder value "[u'INPUT_VALUE_OPTION_UNSPECIFIED', u'RAW', u'USER_ENTERED']"
+
+# How the input data should be inserted.
+insert_data_option = u'OVERWRITE'  # TODO: Update placeholder value.
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+CLIENT_SECRET_FILE = 'client_secret_220360779726-9udn1qo31ie4096mbtg4k0hojog8llil.apps.googleusercontent.com.json'
+APPLICATION_NAME = 'telebot_buh'
+
+LINK_SHEET_BASE = 'https://docs.google.com/spreadsheets/d/'
+
+HELP_MSG = 'If you whant to save some expences  you have to type me : /exp product_name 100.00  comment  '
+UNKNOWN_COMMAND_MSG = 'Sorry, I cant recognse what did you type. type /help to get help'
+ERR_MSG = 'uups... something goes wrong '
+COMMAND_IN_DEV_MSG = 'OMG!!! this commang is in developemt still. May be next sprint...'
+
---
+++ helper/googlehelper.py
@@ -0,0 +1,124 @@
+import config.config as cf
+
+import time
+
+import httplib2
+import os
+
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(cf.CLIENT_SECRET_FILE, cf.SCOPES)
+        flow.user_agent = cf.APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def read_cell_spreadsheet(rangeName):
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http())
+
+	service = discovery.build('sheets', 'v4', http=http,
+							  discoveryServiceUrl=cf.discoveryUrl)
+
+	result = service.spreadsheets().values().get(
+		spreadsheetId=cf.spreadsheetId, range=rangeName).execute()
+	values = result.get('values', [])
+
+
+	if not values:
+		print('No data found.')
+	else:
+		print('Name, Major:')
+		for row in values:
+			# Print columns A and E, which correspond to indices 0 and 4.
+			print('%s' % (row[0]))
+			return row[0]
+
+def read_rows_spreadsheet(rangeName):
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http())
+
+	service = discovery.build('sheets', 'v4', http=http,
+							  discoveryServiceUrl=cf.discoveryUrl)
+
+	result = service.spreadsheets().values().get(
+		spreadsheetId = cf.spreadsheetId, range=rangeName).execute()
+	values = result.get('values', [])
+
+	if not values:
+		print('No data found.')
+	else:
+		return values
+
+def write_spreadsheet(message):
+
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http())
+	message_fields = convert_msg_to_sheet(message)
+	service = discovery.build('sheets', 'v4', http=http,
+							  discoveryServiceUrl=cf.discoveryUrl)
+
+	rangeName = 'A1:F1'
+
+	values = [
+		message_fields  # cell vales
+		# Additional rows ...
+
+	]
+
+	value_range_body = {
+		# TODO: Add desired entries to the request body.
+		'values':values
+	}
+
+	request = service.spreadsheets().values().append(spreadsheetId=cf.spreadsheetId, range=rangeName,
+													 valueInputOption=cf.value_input_option,
+													 insertDataOption=cf.insert_data_option, body=value_range_body)
+	response = request.execute()
+	print(response)
+
+def convert_msg_to_sheet(message):
+	sheet_row = []
+	sheet_row.append(time.strftime('%x %X')) # set datetime
+	sheet_row.append(message.from_user.first_name) #user name
+
+	body = message.text.split(' ')
+
+	sheet_row.append(body[0]) #placeholder for command
+	sheet_row.append(body[1]) #placeholder for product name
+	sheet_row.append(float(body[2])) #placeholder for cost
+	sheet_row.append(' '.join(body[3:])) #placeholder for comment
+
+	return sheet_row
