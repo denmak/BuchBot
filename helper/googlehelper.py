c281ba33415cd26f3980bb95f21b00769d72d42a
denmak <denmak@live.ru>
2017-05-04 00:09:48 +0200

    move back client secret file to main

--- helper/googlehelper.py
+++ helper/googlehelper.py
@@ -5,7 +5,7 @@ import time
 import httplib2
 import os
 
-from apiclient import discovery
+from googleapiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
